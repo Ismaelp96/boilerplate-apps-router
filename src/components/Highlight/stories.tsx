5cf0158026c365c7828d1dd58854085b425529fd
ismaelbelmicro <ismael.patrick@belmicro.com.br>
2023-11-24 12:57:12 -0300

    Highlight Parte 5: Criando opção de diferentes alignments

--- src/components/Highlight/stories.tsx
+++ src/components/Highlight/stories.tsx
@@ -14,9 +14,3 @@ export default {
 } as Meta
 
 export const Default: StoryObj<HighlightProps> = {}
-
-export const WithFloatImage: StoryObj<HighlightProps> = {}
-
-WithFloatImage.args = {
-  floatImage: '/img/red-dead-float'
-}
