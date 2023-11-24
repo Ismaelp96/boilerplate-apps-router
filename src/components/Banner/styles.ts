af0af8dd7de61d52546d8ba652c7486beef7d1b3
ismaelbelmicro <ismael.patrick@belmicro.com.br>
2023-11-24 10:49:50 -0300

    Highlight Parte 2: Criando a estrutura e testes

--- src/components/Banner/styles.ts
+++ src/components/Banner/styles.ts
@@ -1,6 +1,6 @@
 import styled, { css } from 'styled-components'
 import media from 'styled-media-query'
-import * as RibbonStyles from 'components/Ribbon/styles'
+import * as RibbonStyles from '../../components/Ribbon/styles'
 
 export const Wrapper = styled.main`
   position: relative;
